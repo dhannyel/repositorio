0fa4c74571a3392aa543eb6a6e7f32aa64fef997
Daniel Pedraza Vargas <dhannyel06@gmail.com>
2025-04-29 17:33:35 -0500

    Estilos

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -51,7 +51,7 @@
 														</a>
 														<a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
 																href="https://wa.me/{{ $tool->phone }}?text=Hola,%20tengo%20una%20consulta" target="_blank">
-																<x-bi-whatsapp class="shrink-0 size-4" />
+																<x-codicon-mail class="shrink-0 size-4" />
 																Contactanos por WhatsApp
 														</a>
 												</div>
